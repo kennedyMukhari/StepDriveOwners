c2fefdb0896688beecbfecbed5ec6897b2d05733
mmatliThato <50619726+mmatliThato@users.noreply.github.com>
2019-09-10 16:25:35 +0200

    changes

--- src/app/past-b/past-b.page.ts
+++ src/app/past-b/past-b.page.ts
@@ -7,22 +7,23 @@ import * as firebase from 'firebase';
   styleUrls: ['./past-b.page.scss'],
 })
 export class PastBPage implements OnInit {
-  //database
   db = firebase.firestore();
-  //array in a database
-  reviews = [];
+  storage = firebase.firestore();
+  // reviews = [];
+  review= [];
+  newreviews=[];
   constructor(private router: Router) { 
-    this.db.collection('review').onSnapshot(snapshot => {
+    this.db.collection('reviews').onSnapshot(snapshot => {
       snapshot.forEach(doc => {
         // this.users = doc.data();
         // this.addMarkersOnTheCustomersCurrentLocation(this.users.coords.lat, this.users.coords.lng);
-        this.reviews.push(doc.data());
-        console.log('My array is ',this.reviews);
-        this.reviews.forEach(Customers => {
+        this.review.push(doc.data());
+        console.log('My array is ',this.review);
+        this.review.forEach(Customers => {
           console.log('reviews in my array in my array', Customers.schooluid);
           console.log('Owners UID logged in', firebase.auth().currentUser.uid);
           if(Customers.schooluid === firebase.auth().currentUser.uid){
-             this.reviews.push(doc.data())
+             this.review.push(doc.data())
           }
         }) 
 
